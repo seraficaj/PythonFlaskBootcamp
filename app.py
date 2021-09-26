c62dc08182fe3aac7ce43fa24d3d905fc2510331
Jason Serafica <seraficaj@gmail.com>
2021-09-25 19:31:37 -0700

    64. dynamic routing

--- app.py
+++ app.py
@@ -13,5 +13,10 @@ def info():
     return "<h1>Puppies are cute.</h1>"
 
 
+@app.route("/puppy/<name>")
+def puppy(name):
+    return (f"<h1>This is a page for {name.upper()}</h1>)")
+
+
 if __name__ == "__main__":
     app.run()
