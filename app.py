67386da80ddb34068b842fca7fe79035ed774537
Jason Serafica <seraficaj@gmail.com>
2021-09-27 19:39:31 -0700

    69. template variables

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("basic.html")
+    name = "Jose Portilla"
+    letters = list(name)
+    pup_dict = {"pup_name": "Sammy"}
+    return render_template(
+        "basic.html", name=name, letters=letters, pup_dict=pup_dict
+    )
 
 
 if __name__ == "__main__":
