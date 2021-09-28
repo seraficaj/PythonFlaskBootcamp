7b1abf27d23cf9e996741d20e0f121f39374fe10
Jason Serafica <seraficaj@gmail.com>
2021-09-27 19:30:05 -0700

    68. template basics

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello Puppy</h1"
-
-
-@app.route("/information")
-def info():
-    return "<h1>Puppies are cute.</h1>"
-
-
-@app.route("/puppy/<name>")
-def puppy(name):
-    return f"<h1>This is a page for {name.upper()}</h1>)"
-
-@app.route('/puppy_latin/<name>')
-def puppy_latin(name):
-    pupname = ''
-    if name[-1] == 'y':
-        pupname = name[:-1] + 'iful'
-    else:
-        pupname = name + 'y'
-    return f"<h1>Your Puppy Latin Name is {pupname}"
+    return render_template("basic.html")
 
 
 if __name__ == "__main__":
