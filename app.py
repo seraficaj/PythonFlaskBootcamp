3169e93c18a5ecf89c0db910369e5c5ede0f4e14
Jason Serafica <seraficaj@gmail.com>
2021-10-01 20:14:08 -0700

    76. Flask Form Basics

--- app.py
+++ app.py
@@ -1,47 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-@app.route("/")
+class InfoForm(FlaskForm):
+    breed = StringField("What breed are you?")
+    submit = SubmitField("Submit")
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    breed = False
+    form = InfoForm()
+    if form.validate_on_submit():
+        breed = form.breed.data
+        form.breed.data = ''
 
-
-@app.route("/signup_form")
-def signup_form():
-    return render_template("signup.html")
-
-
-@app.route("/thankyou")
-def thankyou():
-    uppercase_letter = False
-    lowercase_letter = False
-    ends_with_num = False
-    meets_reqs = False
-
-    username = request.args.get("username")
-
-    uppercase_letter = any(letter.islower() for letter in username)
-    lowercase_letter = any(letter.isupper() for letter in username)
-    ends_with_num = username[-1].isdigit()
-
-    if lowercase_letter and ends_with_num and uppercase_letter:
-        meets_reqs = True
-
-    return render_template(
-        "thankyou.html",
-        uppercase_letter=uppercase_letter,
-        lowercase_letter=lowercase_letter,
-        ends_with_num=ends_with_num,
-        meets_reqs=meets_reqs,
-    )
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
+    return render_template('index.html', form=form, breed=breed)
 
 if __name__ == "__main__":
     app.run(debug=True)
