e61697e47f64655e2f77deef72e12131ba8243ad
Jason Serafica <seraficaj@gmail.com>
2021-10-02 15:36:56 -0700

    77. form fields part one

--- app.py
+++ app.py
@@ -1,24 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import (
+    StringField,
+    BooleanField,
+    DateTimeField,
+    RadioField,
+    SelectField,
+    TextField,
+    SubmitField,
+)
+from wtforms.fields.simple import TextAreaField
+
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config["SECRET_KEY"] = "mysecretkey"
+
 
 class InfoForm(FlaskForm):
     breed = StringField("What breed are you?")
     submit = SubmitField("Submit")
+    breed = StringField("What breed are you?", validators=[DataRequired()])
+    has_nametag = BooleanField("Does this dog have a nametag?")
+    mood = RadioField(
+        "Please choose your mood:",
+        choices=[("mood_one", "Happy"), ("mood_two", "Excited")],
+    )
+    food_choice = SelectField(
+        u"Pick your favorite food:",
+        choices=[("chi", "Chicken"), ("bf", "beef"), ("fish", "Fish")],
+    )
+    feedback = TextAreaField()
+    submit = SubmitField("Submit")
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    breed = False
     form = InfoForm()
     if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ''
+        session["breed"] = form.breed.data
+        session["has_nametag"] = form.has_nametag.data
+        session["mood"] = form.mood.data
+        session["food"] = form.food_choice.data
+        session["feedback"] = form.feedback.data
 
-    return render_template('index.html', form=form, breed=breed)
+        return redirect(url_for("thankyou"))
+
+    return render_template("index.html", form=form)
+
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
