4ba76a7001e78aaf3955de5f601bf069491a917a
Jason Serafica <seraficaj@gmail.com>
2021-10-02 16:05:41 -0700

    78. Flask Form Fields Pt. 2

--- app.py
+++ app.py
@@ -19,36 +19,35 @@ app.config["SECRET_KEY"] = "mysecretkey"
 
 
 class InfoForm(FlaskForm):
-    breed = StringField("What breed are you?")
-    submit = SubmitField("Submit")
-    breed = StringField("What breed are you?", validators=[DataRequired()])
+    breed = StringField('What breed are you?',validators=[DataRequired()])
     has_nametag = BooleanField("Does this dog have a nametag?")
-    mood = RadioField(
-        "Please choose your mood:",
-        choices=[("mood_one", "Happy"), ("mood_two", "Excited")],
-    )
-    food_choice = SelectField(
-        u"Pick your favorite food:",
-        choices=[("chi", "Chicken"), ("bf", "beef"), ("fish", "Fish")],
-    )
+    mood = RadioField('Please choose your mood:', choices=[('mood_one','Happy'),('mood_two','Excited')])
+    food_choice = SelectField(u'Pick Your Favorite Food:',
+                          choices=[('chi', 'Chicken'), ('bf', 'Beef'),
+                                   ('fish', 'Fish')])
     feedback = TextAreaField()
-    submit = SubmitField("Submit")
+    submit = SubmitField('Submit')
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
+    # Create instance of the form.
     form = InfoForm()
+    # If the form is valid on submission (we'll talk about validation next)
     if form.validate_on_submit():
-        session["breed"] = form.breed.data
-        session["has_nametag"] = form.has_nametag.data
-        session["mood"] = form.mood.data
-        session["food"] = form.food_choice.data
-        session["feedback"] = form.feedback.data
+        # Grab the data from the breed on the form.
+
+        session['breed'] = form.breed.data
+        session['has_nametag'] = form.has_nametag.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
 
         return redirect(url_for("thankyou"))
 
-    return render_template("index.html", form=form)
 
+    return render_template('index.html', form=form)
 
 @app.route("/thankyou")
 def thankyou():
