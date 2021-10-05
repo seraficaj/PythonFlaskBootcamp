bc208d79e062c92cd5ed0d3d80f31a973e86bb3c
Jason Serafica <seraficaj@gmail.com>
2021-10-05 09:38:57 -0700

    84. flask migrations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,6 +14,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+Migrate(app,db)
+
 
 class Puppy(db.Model):
     __tablename__ = "puppies"
@@ -20,10 +23,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     def __repr__(self):
         return f"Puppy {self.name} is {self.age} year/s old"
