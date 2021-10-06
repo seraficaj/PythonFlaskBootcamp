5e7e16cacdea7f4a1b8e2f2a66d0aa98e2112eac
Jason Serafica <seraficaj@gmail.com>
2021-10-06 07:22:04 -0700

    86. DBs in Views Pt 2. View Functions

--- adoption_site.py
+++ adoption_site.py
@@ -1,6 +1,6 @@
 import os
 
-# from forms import AddForm, DelForm
+from forms import AddForm, DelForm
 
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -35,3 +35,45 @@ class Puppy(db.Model):
     def __repr__(self):
         return f'Puppy name: {self.name}'
 
+
+## VIEW FUNCS 
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pup():
+    form = AddForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        new_puppy = Puppy(name)
+        db.session.add(new_puppy)
+        db.session.commit()
+        
+        return redirect(url_for('list_puppies'))
+    return render_template('add.html', form=form)
+
+
+@app.route('list')
+def list_puppies():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def del_pup():
+    form = DelForm()
+    if form.validate_on_submit():
+        id = form.id.data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_puppies'))
+    return render_template('delete.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
