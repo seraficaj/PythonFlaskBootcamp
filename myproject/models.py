f832ff18731183dca2310a2da0a21a3dbb017a0d
Jason Serafica <seraficaj@gmail.com>
2021-10-13 06:57:43 -0700

    98. Flask Login pt. 1

--- myproject/models.py
+++ myproject/models.py
@@ -1,33 +1,23 @@
-from myproject import db
+from myproject import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class Puppy(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-    __tablename__ = 'puppies'
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.Text)
-    owner = db.relationship('Owner',backref='puppy',uselist=False)
 
-    def __init__(self,name):
-        self.name = name
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
 
-    def __repr__(self):
-        if self.owner:
-            return f"Puppy name is {self.name} and owner is {self.owner.name}"
-        else:
-            return f"Puppy name is {self.name} and has no owner assigned yet."
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
 
-class Owner(db.Model):
-
-    __tablename__ = 'owners'
-
-    id = db.Column(db.Integer,primary_key= True)
-    name = db.Column(db.Text)
-    # We use puppies.id because __tablename__='puppies'
-    puppy_id = db.Column(db.Integer,db.ForeignKey('puppies.id'))
-
-    def __init__(self,name,puppy_id):
-        self.name = name
-        self.puppy_id = puppy_id
-
-    def __repr__(self):
-        return f"Owner Name: {self.name}"
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
