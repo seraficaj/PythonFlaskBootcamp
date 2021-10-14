e971b41043db0e6148044f31a0bb1d02e555205d
Jason Serafica <seraficaj@gmail.com>
2021-10-14 07:16:42 -0700

    101. Flask Login pt. 4 no flashes

--- app.py
+++ app.py
@@ -1,63 +1,73 @@
-from myproject import app, db
-from flask import render_template, redirect, request, url_for, flash, abort
-from flask_login import login_user, login_required, logout_user
+from myproject import app,db
+from flask import render_template, redirect, request, url_for, flash,abort
+from flask_login import login_user,login_required,logout_user
 from myproject.models import User
 from myproject.forms import LoginForm, RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
 
-@app.route("/welcome")
+@app.route('/welcome')
 @login_required
 def welcome_user():
-    return render_template("welcome_user.html")
+    return render_template('welcome_user.html')
 
-
-@app.route("/logout")
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash("You've been logged out.")
-    return redirect(url_for("home"))
+    flash('You logged out!')
+    return redirect(url_for('home'))
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+
     form = LoginForm()
     if form.validate_on_submit():
+        # Grab the user from our User Models table
         user = User.query.filter_by(email=form.email.data).first()
+        
+        # Check that the user was supplied and the password is right
+        # The verify_password method comes from the User object
+        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
 
         if user.check_password(form.password.data) and user is not None:
+            #Log in the user
+            print(user)
             login_user(user)
-            flash("Logged in successfully!")
+            flash('Logged in successfully.')
 
-            next = request.args.get("next")
+            # If a user was trying to visit a page that requires a login
+            # flask saves that URL as 'next'.
+            next = request.args.get('next')
 
-            if next == None or not next[0] == "/":
-                next = url_for("welcome_user")
+            # So let's now check if that next exists, otherwise we'll go to
+            # the welcome page.
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
 
             return redirect(next)
+    return render_template('login.html', form=form)
 
-    return render_template("login.html", form=form)
-
-
-@app.route("/register", methods=["GET", "POST"])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        user = User(
-            email=form.email.data, user=form.user.data, password=form.password.data
-        )
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+
         db.session.add(user)
         db.session.commit()
-        flash("Thanks for registration!")
-        return redirect(url_for("login"))
-    return render_template("register.html", form=form)
+        flash('Thanks for registering! Now you can login!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
