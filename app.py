e99d24aa2dd37e432a1def7ce112d204a88b0ada
Jason Serafica <seraficaj@gmail.com>
2021-10-18 20:29:15 -0700

    103. flask OAuth with Google Pt. 1

--- app.py
+++ app.py
@@ -1,73 +1,45 @@
-from myproject import app,db
-from flask import render_template, redirect, request, url_for, flash,abort
-from flask_login import login_user,login_required,logout_user
-from myproject.models import User
-from myproject.forms import LoginForm, RegistrationForm
-from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+
+from flask import Flask, redirect, url_for, render_template
+from flask_dance.contrib.google import make_google_blueprint, google
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secretkeylol"
+
+blueprint = make_google_blueprint(
+    client_id="", 
+    client_secret="", 
+    offline=True, 
+    scope=["profile", "email"]
+)
+
+app.register_blueprint(blueprint, url_prefix='/login')
 
 @app.route('/')
-def home():
+def index():
     return render_template('home.html')
 
 
 @app.route('/welcome')
-@login_required
-def welcome_user():
-    return render_template('welcome_user.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You logged out!')
-    return redirect(url_for('home'))
+def welcome():
+    # return Internal Sever Error if not logged in
+    if not google.authorized:
+        return render_template(url_for('google.login'))
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok, resp.text 
+    email = resp.json()['email']
+    return render_template('welcome.html', email=email)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/google')
 def login():
+    if not google.authorized:
+        return render_template(url_for('google.login'))
+    resp = google.get('/oauth2/v2/userinfo')
+    assert resp.ok, resp.text 
+    email = resp.json()['email']
 
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Grab the user from our User Models table
-        user = User.query.filter_by(email=form.email.data).first()
-        print(user)
-        # Check that the user was supplied and the password is right
-        # The verify_password method comes from the User object
-        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
-
-        if user is not None and user.check_password(form.password.data):
-            #Log in the user
-
-            login_user(user)
-            flash('Logged in successfully.')
-
-            # If a user was trying to visit a page that requires a login
-            # flask saves that URL as 'next'.
-            next = request.args.get('next')
-
-            # So let's now check if that next exists, otherwise we'll go to
-            # the welcome page.
-            if next == None or not next[0]=='/':
-                next = url_for('welcome_user')
-
-            return redirect(next)
-    return render_template('login.html', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-
-    if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data,
-                    password=form.password.data)
-
-        db.session.add(user)
-        db.session.commit()
-        flash('Thanks for registering! Now you can login!')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('welcome.html', email=email)
